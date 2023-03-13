88d0849831ceca9808c6fb2270c5315452186788
erkam_home <erkam.uzuncayir@fauderfield.com>
2023-03-13 21:51:01 +0300

    Create solution of Exercise_2_40

---
+++ Chapter_2/Exercise_2_40.cpp
@@ -0,0 +1,19 @@
+/* Chapter 2 - Exercise 40
+
+Exercise 2.40: Write your own version of the Sales_data class.
+
+*/
+
+#include <iostream>
+
+struct Sales_data 
+{ 
+    std::string bookName;
+    double saleRatio = 0.0;
+    std::string writtenBy;
+};
+
+int main()
+{
+    return 0;
+}
